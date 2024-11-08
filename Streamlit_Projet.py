8678d667995b2e9a7db8fe7481262f9c7399a1e8
Eric-Potiron <eric.potiron@gmail.com>
2024-11-08 03:53:07 +0100

    Update Streamlit_Projet.py

--- Streamlit_Projet.py
+++ Streamlit_Projet.py
@@ -698,7 +698,7 @@ elif page == pages[4]:
         buffer = io.StringIO()
         final_df.info(buf=buffer)
         s = buffer.getvalue()
-        st.text(s)
+        st.code(s)
 
     elif option == "Valeurs manquantes":
         final_df = load_csv('final_df.csv', header=0)
